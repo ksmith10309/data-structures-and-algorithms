4ccb2ca2b698c74b35fa1765d2643d6ead652e9c
Katherine Smith <katherine.lo.smith@gmail.com>
2018-10-22 15:20:55 -0700

    Complete Code Challenge 06

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -11,35 +11,66 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
-}
 
-LinkedList.prototype.insert = function(value) {
-  let newNode = new Node(value);
-  newNode.next = this.head;
-  this.head = newNode;
-}
-
-LinkedList.prototype.includes = function(value) {
-  let currentNode = this.head;
-  if (currentNode === null) return false;
-  else if (currentNode.value === value) return true;
-  while (currentNode.next !== null) {
-    currentNode = currentNode.next;
-    if (currentNode.value === value) return true;
+  insert(value) {
+    let newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
   }
-  return false;
-}
 
-LinkedList.prototype.print = function() {
-  let str = '';
-  let currentNode = this.head;
-  if (currentNode === null) return str;
-  else str += currentNode.value;
-  while (currentNode.next !== null) {
-    currentNode = currentNode.next;
-    str += ', ' + currentNode.value;
+  includes(value) {
+    let currentNode = this.head;
+    if (currentNode === null) return false;
+    else if (currentNode.value === value) return true;
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+      if (currentNode.value === value) return true;
+    }
+    return false;
   }
-  return str;
+
+  print() {
+    let str = '';
+    let currentNode = this.head;
+    if (currentNode === null) return str;
+    else str += currentNode.value;
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+      str += ', ' + currentNode.value;
+    }
+    return str;
+  }
+
+  append(value) {
+    let currentNode = this.head;
+    while(currentNode.next !== null) {
+      currentNode = currentNode.next;
+    }
+    let newNode = new Node(value);
+    newNode.next = null;
+    currentNode.next = newNode;
+  }
+
+  insertBefore(value, newVal) {
+    let currentNode = this.head;
+    while(currentNode.next.value !== value) {
+      currentNode = currentNode.next;
+    }
+    let newNode = new Node(newVal);
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+  }
+
+  insertAfter(value, newVal) {
+    let currentNode = this.head;
+    while(currentNode.value !== value) {
+      currentNode = currentNode.next;
+    }
+    let newNode = new Node(newVal);
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+  }
+
 }
 
 module.exports = LinkedList;
--- linked-list/linked-list.test.js
+++ linked-list/linked-list.test.js
@@ -45,3 +45,63 @@ describe('Testing LinkedList print method', () => {
     expect(testlist.print()).toBe('C, B, A');
   });
 });
+
+const testlist2 = new LinkedList();
+testlist2.insert('C');
+testlist2.insert('B');
+testlist2.insert('A');
+
+describe('Testing LinkedList append method', () => {
+  it('should print A, B, C, D after appending D', () => {
+    testlist2.append('D');
+    expect(testlist2.print()).toBe('A, B, C, D');
+  });
+  it('should print A, B, C, D, E after appending E', () => {
+    testlist2.append('E');
+    expect(testlist2.print()).toBe('A, B, C, D, E');
+  });
+  it('should print A, B, C, D, E, F after appending F', () => {
+    testlist2.append('F');
+    expect(testlist2.print()).toBe('A, B, C, D, E, F');
+  });
+});
+
+const testlist3 = new LinkedList();
+testlist3.insert('A');
+testlist3.insert('B');
+testlist3.insert('C');
+
+describe('Testing LinkedList insertBefore method', () => {
+  it('should print C, D, B, A after inserting D before B', () => {
+    testlist3.insertBefore('B', 'D');
+    expect(testlist3.print()).toBe('C, D, B, A');
+  });
+  it('should print C, D, E, B, A after inserting E before B', () => {
+    testlist3.insertBefore('B', 'E');
+    expect(testlist3.print()).toBe('C, D, E, B, A');
+  });
+  it('should print C, D, E, B, F, A after inserting F before A', () => {
+    testlist3.insertBefore('A', 'F');
+    expect(testlist3.print()).toBe('C, D, E, B, F, A');
+  });
+});
+
+const testlist4 = new LinkedList();
+testlist4.insert('C');
+testlist4.insert('B');
+testlist4.insert('A');
+
+describe('Testing LinkedList insertAfter method', () => {
+  it('should print A, B, D, C after inserting D after B', () => {
+    testlist4.insertAfter('B', 'D');
+    expect(testlist4.print()).toBe('A, B, D, C');
+  });
+  it('should print A, B, E, D, C after inserting E after B', () => {
+    testlist4.insertAfter('B', 'E');
+    expect(testlist4.print()).toBe('A, B, E, D, C');
+  });
+  it('should print A, F, B, E, D, C after insert F after C', () => {
+    testlist4.insertAfter('A', 'F');
+    expect(testlist4.print()).toBe('A, F, B, E, D, C');
+  });
+});
